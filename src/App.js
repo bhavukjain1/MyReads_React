158c6bd3ba0f5c435345e50e2967267751e6d4be
Bhavuk Jain <bhavuk_92@yahoo.co.in>
2017-10-28 23:14:25 +0530

    Fetching books from the API.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react';
 import BookShelf from './BookShelf'
 import './App.css';
+import * as BooksAPI from './BooksAPI'
 
 class App extends Component {
 
 
+  state = {
+    wantToRead:[],
+    currentlyReading:[],
+    read:[]
+  }
+
+  componentDidMount() {
+
+     BooksAPI.getAll().then(books => {
+          this.setState({
+            wantToRead: books.filter(book => book.shelf === 'wantToRead'),
+            currentlyReading: books.filter(book => book.shelf === 'currentlyReading'),
+            read: books.filter(book => book.shelf === 'read')
+          })
+     })
+
+  }
+
   render() {
 
-    const bookShelves = [
+    const bookShelves =
       {
         type:'Want to Read',
         books:[
@@ -16,7 +35,9 @@ class App extends Component {
             image:'http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api'}
             ]
       }
-    ]
+
+
+    // const bookShelf =
 
     console.log(bookShelves)
 
@@ -26,14 +47,24 @@ class App extends Component {
           <h1>MyReads</h1>
         </div>
         <div className='list-books-content'>
-           {bookShelves.map(bookShelf => (
             <div>
               <BookShelf
-                type={bookShelf.type}
-                books={bookShelf.books}
+                type={'Want To Read'}
+                books={this.state.wantToRead}
               />
             </div>
-           ))}
+            <div>
+              <BookShelf
+                type={'Currently Reading'}
+                books={this.state.currentlyReading}
+              />
+            </div>
+            <div>
+              <BookShelf
+                type={'Read'}
+                books={this.state.read}
+              />
+            </div>
         </div>
       </div>
     )
--- src/Book.js
+++ src/Book.js
@@ -9,11 +9,11 @@ class Book extends Component {
 		return (
 			<div className='book'>
 			   <div className='book-top'>
-			      <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.image})` }}></div>
+			      <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
                   <SelectOption />
 			   </div>
 			   <div className="book-title">{book.title}</div>
-               <div className="book-authors">{book.author}</div>
+               <div className="book-authors">{book.publisher}</div>
 			</div>
 			)
 	}
